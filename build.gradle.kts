a024ab8e9788f78c823523a6cba899cab58c938f
garry183 <garry183@gmail.com>
2026-01-29 18:14:06 +0530

    Add Gradle configuration files

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+    id("com.android.library") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+}
